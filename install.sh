2957736b006e6b4cd64acf31605c9ea57810aab2
Leander Janssen <leander@itforge.nl>
2014-07-14 22:16:05 +0200

    Small enhancement to install.sh to keep git repo in sync on remote

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 echo "Linking dotfiles in ~/"
 chmod +x ./makesymlinks.sh
 ./makesymlinks.sh
+# Remove +x bit again to prevent makesymlinks.sh going out of sync with git repo
+chmod -x ./makesynlinks.sh
 
 echo "Installing Vim plugins..."
 git clone git://github.com/gmarik/Vundle.vim.git ./vim/bundle/vundle
