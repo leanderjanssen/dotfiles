430448f5c03737778f6b0b635ec3b54d69d9fb42
Leander Janssen <leander@itforge.nl>
2014-07-14 22:07:00 +0200

    Fixed typo in makesymlinks.sh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 ########## Variables
 
 dir=~/dotfiles                  # dotfiles directory
-lddir=~/dotfiles_old            # old dotfiles backup directory
+olddir=~/dotfiles_old            # old dotfiles backup directory
 files="dircolors tmux.conf vimrc vim zshrc zsh_prompt"    # list of files/folders to symlink in homedir
 
 ##########
@@ -26,7 +26,7 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
+    mv ~/.$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
