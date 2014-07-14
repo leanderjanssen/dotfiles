9381f6a61d7b33dea9669acf5815dc76b5b8222b
Leander Janssen <leander@itforge.nl>
2014-07-14 22:20:46 +0200

    Fixed type in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Linking dotfiles in ~/"
 chmod +x ./makesymlinks.sh
 ./makesymlinks.sh
 # Remove +x bit again to prevent makesymlinks.sh going out of sync with git repo
-chmod -x ./makesynlinks.sh
+chmod -x ./makesymlinks.sh
 
 echo "Installing Vim plugins..."
 git clone git://github.com/gmarik/Vundle.vim.git ./vim/bundle/vundle
